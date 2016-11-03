6166199dc29a937e0801599fbfc06ced641bb937
Eduardo Rocha <eduardorocha@cds.eb.mil.br>
2016-11-03 15:08:16 -0200

    get and post managent using forms example

---
+++ expressHelloWorld/README.md
@@ -0,0 +1,6 @@
+just remember to install all dependences
+
+$ npm install express
+$ npm install body-parser
+$ npm install cookie-parser
+$ npm install multer
--- expressHelloWorld/server.js
+++ expressHelloWorld/server.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+    res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
 
 app.get('/', function (req, res) {
     console.log('Got a GET request for the homepage');
